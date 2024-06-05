5bc7e393e697c0da265cd89d2871bf383aa81f7d
Júlio Ferreira <juliolima731@gmail.com>
2024-06-04 23:12:13 -0400

    feat: adding new workouts page structure

---
+++ src/pages/workout/new.tsx
@@ -0,0 +1,14 @@
+// pages/workout/new.tsx
+import React from 'react';
+
+const NewExercise: React.FC = () => {
+    return (
+        <div>
+            <h1>New Exercise</h1>
+            <p>This is the page to create a new exercise.</p>
+            {/* Formulário ou componentes adicionais para criar um novo exercício */}
+        </div>
+    );
+};
+
+export default NewExercise;
